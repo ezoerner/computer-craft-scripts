ce1efd99b16917b1654ec3749a6895954502c819
Eric Zoerner <ezoerner@ebuddy.com>
2013-09-17 18:58:21 +0200

    reorganization

--- vertical-mineshaft.lua
+++ vertical-mineshaft.lua
@@ -238,8 +238,7 @@ end
 local function pillarUp(n)
     -- collect ladders?
     for n=1,n do
-        tryUp()
-        if selectCobblestone() then
+        if tryUp() and selectCobblestone() then
             turtle.placeDown()
         end
     end
@@ -264,43 +263,55 @@ if not refuel() then
     return
 end
 
+local function digBottomArea()
+    if not tryForwards() then
+        print("Could not go forwards")
+        return false
+    end
+
+    if not tryUp() then
+        print ("Could not go up")
+        return false
+    end
+
+    if not tryForwards() then
+        print("Could not go forwards")
+        return false
+    end
+
+    -- go down one plus three more for the water pit
+    for n=1,4 do
+        if not tryDown() then
+            print("Could not go down")
+            return false
+        end
+    end
+    return true
+end
+
+local function placeWaterPit()
+    -- for now place the three water buckets
+    -- an improvement later will be to use only two buckets to create
+    -- an infinite water trough and use that to generate three water
+    for n=1,3 do
+        if not tryUp() then
+            return false
+        end
+        if not turtle.select(12+n) or not turtle.placeDown() then
+            return false
+        end
+    end
+    return true
+end
+
+local function digUtilityRoom()
+
+end
+
 digAlcove()
 digAscentShaft()
 pillarUp(3)
-
-if not tryForwards() then
-    print("Could not go forwards")
-    return
-end
-
-if not tryUp() then
-    print ("Could not go up")
-    return
-end
-
-if not tryForwards() then
-    print("Could not go forwards")
-    return
-end
-
--- go down one plus three more for the water pit
-for n=1,4 do
-    if not tryDown() then
-        print("Could not go down")
-        return
-    end
-end
-
--- for now place the three water buckets
--- an improvement later will be to use only two buckets to create
--- an infinite water trough and use that to generate three water
-for n=1,3 do
-    if not tryUp() then
-        return
-    end
-    if not turtle.select(12+n) or not turtle.placeDown() then
-        return
-    end
-end
-
+digBottomArea()
+placeWaterPit()
+digUtilityRoom()
 digToSurface()
